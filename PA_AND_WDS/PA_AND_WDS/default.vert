53a2e48153873ca8166e34a289717cdbddc4d211
FlashBang2 <maconnowosci@gmail.com>
2022-10-09 13:55:03 +0200

    Added Camera and glm library

--- PA_AND_WDS/PA_AND_WDS/default.vert
+++ PA_AND_WDS/PA_AND_WDS/default.vert
@@ -7,9 +7,11 @@ out vec3 color;
 
 out vec2 texCoords;
 
+uniform mat4 camMatrix;
+
 void main()
 {
-	gl_Position = vec4(aPos.x,aPos.y,aPos.z, 1.0);
+	gl_Position = camMatrix * vec4(aPos, 1.0);
 	color = aColor;
 	texCoords = aTex;
 }
--- PA_AND_WDS/PA_AND_WDS/main.cpp
+++ PA_AND_WDS/PA_AND_WDS/main.cpp
@@ -2,12 +2,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "texture.h"
 #include "shader.h"
 #include "VAO.h"
 #include "VBO.h"
 #include "EABO.h"
+#include "Camera.h"
 
 int main() 
 {
